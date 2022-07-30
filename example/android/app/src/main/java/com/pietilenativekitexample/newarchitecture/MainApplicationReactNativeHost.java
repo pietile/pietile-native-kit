baf8effb57c28681091cd8c077de166b8f8b5ec9
Vladislav Shabanov <vshabanov88@gmail.com>
2022-07-31 03:05:19 +0700

    Fix example

--- example/android/app/src/main/java/com/pietilenativekitexample/newarchitecture/MainApplicationReactNativeHost.java
+++ example/android/app/src/main/java/com/pietilenativekitexample/newarchitecture/MainApplicationReactNativeHost.java
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.rndiffapp.BuildConfig;
-import com.rndiffapp.newarchitecture.components.MainComponentsRegistry;
-import com.rndiffapp.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.pietilenativekitexample.BuildConfig;
+import com.pietilenativekitexample.newarchitecture.components.MainComponentsRegistry;
+import com.pietilenativekitexample.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
--- example/android/app/src/main/java/com/pietilenativekitexample/newarchitecture/MainComponentsRegistry.java
+++ example/android/app/src/main/java/com/pietilenativekitexample/newarchitecture/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.rndiffapp.newarchitecture.components;
+package com.pietilenativekitexample.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
